607b0f3305170ae51b8df892cda7537169d36cd9
SilverNine <eungoo.jung@yellomobile.com>
2016-02-27 22:25:53 +0900

    pray board data binding

--- config/database.js
+++ config/database.js
@@ -1,11 +1,24 @@
 var mysql = require('mysql');
+var express = require('express');
+var path = require('path');
+var app = express();
+var pool;
 
-var pool = mysql.createPool({
-    host: 'localhost',
-    user: 'dev',
-    password: 'roqkfxla',
-    database: 'SILVERNINE'
-});
+if(app.get('env') === 'production') {
+    pool = mysql.createPool({
+        host: 'localhost',
+        user: 'dev',
+        password: 'roqkfxla',
+        database: 'SILVERNINE'
+    });
+} else {
+    pool = mysql.createPool({
+        host: 'lunchchart.com',
+        user: 'dev',
+        password: 'roqkfxla',
+        database: 'SILVERNINE'
+    });
+}
 
 module.exports = {
     url : "mongodb://dev:dev@lunchchart.com:27017/lunchchart"
