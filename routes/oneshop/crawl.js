f36a3a6bef5f5a5333243eb9eaac86a374f043cb
SilverNine <eungoo.jung@yellomobile.com>
2016-05-05 09:42:30 +0900

    oneshop crawl selector change

--- routes/oneshop/crawl.js
+++ routes/oneshop/crawl.js
@@ -10,7 +10,7 @@ exports.list = function(req, res){
 
         var $ = cheerio.load(body);
 
-          $(".good_list > li > a").each(function(i) {
+          $(".uniformShop > .good_list > li > a").each(function(i) {
               var goods = {
                   link : $(this).attr("href")
                   ,imagePath : "http://www.oneshop.co.kr/" + $(this).find("img").attr("src")
