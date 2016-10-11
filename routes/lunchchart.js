9c25c09ec453c7110ba4548cba3bff999d1c63e9
eg <eungoo.jung@gameone.kr>
2016-10-11 10:05:26 +0900

    naver map 추가

--- routes/lunchchart.js
+++ routes/lunchchart.js
@@ -1,5 +1,23 @@
 module.exports = function(app) {
     app.get('/gameone', function(req, res){
-        res.render('lunchchart/index', {});
+
+        var mapkey = "";
+
+        if( process.env.NODE_ENV == 'production' ) {
+            mapkey = "ZsJ3ILQMDIbSGASTvqJI";
+        } else {
+            mapkey = "Z343bXpadHrn2C7KXBUp";
+        }
+
+        /*
+         운영 : ZsJ3ILQMDIbSGASTvqJI
+         개발 : Z343bXpadHrn2C7KXBUp
+         */
+
+        res.render('lunchchart/index', {mapkey : "Z343bXpadHrn2C7KXBUp"});
+    });
+
+    app.get('/sample', function(req, res){
+        res.render('lunchchart/sample', {});
     });
 }
